f03ec1af1c269c79dba64caf92cb4191627e890c
MarcoCipolletta <marcocipolletta1998@gmail.com>
2025-01-27 10:01:24 +0100

    First Commit

---
+++ hair_salon/src/test/java/it/epicode/hair_salon/HairSalonApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.hair_salon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HairSalonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
